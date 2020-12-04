bde52b43b9628eefc20467e25f7f48c81030d679
Onebear <9955066544@qq.com>
2020-12-04 09:43:20 +0800

    修改损失，训练10次 loss = loss(x, adj) + alpha * loss(x, adj2)

--- train.py
+++ train.py
@@ -45,77 +45,78 @@ if args.cuda:
 # Load data
 adj, adj2, features, labels, idx_train, idx_val, idx_test = load_data()
 
-# Model and optimizer
-model = GCN(nfeat=features.shape[1],
-            nhid=args.hidden,
-            nclass=labels.max().item() + 1,
-            dropout=args.dropout)
-
-optimizer = optim.Adam(model.parameters(),
-                       lr=args.lr, weight_decay=args.weight_decay)
-
-
-if args.cuda:
-    model.cuda()
-    features = features.cuda()
-    adj = adj.cuda()
-    adj2 = adj2.cuda()
-    labels = labels.cuda()
-    idx_train = idx_train.cuda()
-    idx_val = idx_val.cuda()
-    idx_test = idx_test.cuda()
-
-
-def train(epoch, alpha):
-    t = time.time()
-    model.train()
-    optimizer.zero_grad()
-
-    output = model(features, adj)
-    loss_train = F.nll_loss(output[idx_train], labels[idx_train])
-
-    output2 = model(features, adj2)
-    loss_train2 = F.nll_loss(output2[idx_train], labels[idx_train])
-
-    loss_train = loss_train + alpha * loss_train2
-    acc_train = accuracy(output[idx_train], labels[idx_train])
-
-    loss_train.backward()
-    optimizer.step()
-
-    if not args.fastmode:
-        # Evaluate validation set performance separately,
-        # deactivates dropout during validation run.
-        model.eval()
-        output = model(features, adj)
-
-    loss_val = F.nll_loss(output[idx_val], labels[idx_val])
-    acc_val = accuracy(output[idx_val], labels[idx_val])
-    print('Epoch: {:04d}'.format(epoch + 1),
-          'loss_train: {:.4f}'.format(loss_train.item()),
-          'acc_train: {:.4f}'.format(acc_train.item()),
-          'loss_val: {:.4f}'.format(loss_val.item()),
-          'acc_val: {:.4f}'.format(acc_val.item()),
-          'time: {:.4f}s'.format(time.time() - t))
-
-
-def test(acc_results, loss_results):
-    model.eval()
-    output = model(features, adj)
-    loss_test = F.nll_loss(output[idx_test], labels[idx_test])
-    acc_test = accuracy(output[idx_test], labels[idx_test])
-    print("Test set results:",
-          "loss= {:.4f}".format(loss_test.item()),
-          "accuracy= {:.4f}".format(acc_test.item()))
-
-    acc_results.append(":.4f".format(acc_test.item()))
-    loss_results.append("{:.4f}".format(loss_test.item()))
-
-
 acc_results = []
 loss_results = []
 time_results = []
+
 for i in range(10):
+    # Model and optimizer
+    model = GCN(nfeat=features.shape[1],
+                nhid=args.hidden,
+                nclass=labels.max().item() + 1,
+                dropout=args.dropout)
+
+    optimizer = optim.Adam(model.parameters(),
+                           lr=args.lr, weight_decay=args.weight_decay)
+
+
+    if args.cuda:
+        model.cuda()
+        features = features.cuda()
+        adj = adj.cuda()
+        adj2 = adj2.cuda()
+        labels = labels.cuda()
+        idx_train = idx_train.cuda()
+        idx_val = idx_val.cuda()
+        idx_test = idx_test.cuda()
+
+
+    def train(epoch, alpha):
+        t = time.time()
+        model.train()
+        optimizer.zero_grad()
+
+        output = model(features, adj)
+        loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+
+        output2 = model(features, adj2)
+        loss_train2 = F.nll_loss(output2[idx_train], labels[idx_train])
+
+        loss_train = loss_train + alpha * loss_train2
+        acc_train = accuracy(output[idx_train], labels[idx_train])
+
+        loss_train.backward()
+        optimizer.step()
+
+        if not args.fastmode:
+            # Evaluate validation set performance separately,
+            # deactivates dropout during validation run.
+            model.eval()
+            output = model(features, adj)
+
+        loss_val = F.nll_loss(output[idx_val], labels[idx_val])
+        acc_val = accuracy(output[idx_val], labels[idx_val])
+        print('Epoch: {:04d}'.format(epoch + 1),
+              'loss_train: {:.4f}'.format(loss_train.item()),
+              'acc_train: {:.4f}'.format(acc_train.item()),
+              'loss_val: {:.4f}'.format(loss_val.item()),
+              'acc_val: {:.4f}'.format(acc_val.item()),
+              'time: {:.4f}s'.format(time.time() - t))
+
+
+    def test(acc_results, loss_results):
+        model.eval()
+        output = model(features, adj)
+        loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+        acc_test = accuracy(output[idx_test], labels[idx_test])
+        print("Test set results:",
+              "loss= {:.4f}".format(loss_test.item()),
+              "accuracy= {:.4f}".format(acc_test.item()))
+
+        acc_results.append("{:.4f}".format(acc_test.item()))
+        loss_results.append("{:.4f}".format(loss_test.item()))
+
+
 
     # Train model
     t_total = time.time()
